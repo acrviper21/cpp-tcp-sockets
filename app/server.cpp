cadfd5ebc682faa1ec1802736ba1e582d44bf0c3
acrviper21 <42522349+acrviper21@users.noreply.github.com>
2026-03-27 22:17:08 -0500

    setup server

--- app/server.cpp
+++ app/server.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 #include <boost/asio.hpp>
 
+int getPortNumber();
+
 int main()
 {
+    namespace ip = boost::asio::ip;
+    int portNumber = getPortNumber();
+
+    try
+    {
+        boost::asio::io_context io_context;
+        ip::tcp::acceptor acceptor(io_context, ip::tcp::endpoint(ip::tcp::v4(), portNumber));
+
+        while(true)
+        {
+            ip::tcp::socket socket(io_context);
+            acceptor.accept(socket);
+
+            std::cout << "Client Connected" << std::endl;
+
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
 
     return 0;
+}
+
+int getPortNumber()
+{
+    int portNumber;
+
+    std::cout << "Which port number do you want to use: ";
+    std::cin >> portNumber;
+
+    return portNumber;
 }
