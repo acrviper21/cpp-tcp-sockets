de0437b630fa3848b0f3c5b596db3ae664291fc6
acrviper21 <42522349+acrviper21@users.noreply.github.com>
2026-03-28 20:57:53 -0500

    update client/server to send message with header size

--- app/server.cpp
+++ app/server.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include <boost/asio.hpp>
 
 int getPortNumber();
@@ -18,7 +20,21 @@ int main()
             ip::tcp::socket socket(io_context);
             acceptor.accept(socket);
 
-            std::cout << "Client Connected" << std::endl;
+            std::string server_response = "Message received";
+            int client_message_size;
+
+            boost::system::error_code error_code;
+            std::vector<char> receive_buffer(1024);
+
+            boost::asio::read(socket, boost::asio::buffer(&client_message_size, sizeof(client_message_size)));
+            receive_buffer.resize(client_message_size);
+            size_t reply_length = boost::asio::read(socket, boost::asio::buffer(receive_buffer), error_code);
+
+            std::cout.write(receive_buffer.data(), reply_length) << std::endl;
+
+            int send_message_size = server_response.size();
+            boost::asio::write(socket, boost::asio::buffer(&send_message_size, sizeof(send_message_size)), error_code);
+            boost::asio::write(socket, boost::asio::buffer(server_response), error_code);
 
         }
     }
